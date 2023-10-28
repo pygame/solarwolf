a49b9b3622481c1a554d67fb47616b4c11c8f554
René Dudfield <renesd@gmail.com>
2023-10-28 23:49:06 +0200

    1.6.0

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setup(
     # package_data={'solarwolf': []},
     url='https://github.com/pygame/solarwolf',
     install_requires=['pygame'],
-    version='1.6.0.dev7',
+    version='1.6.0',
     entry_points={
         'console_scripts': [
             'solarwolf=solarwolf.cli:main',
